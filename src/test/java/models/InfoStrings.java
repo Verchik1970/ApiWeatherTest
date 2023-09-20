191fe273e8f41634e268d04db2266ff6733f9c09
Verchik1970 <132815870+Verchik1970@users.noreply.github.com>
2023-09-20 16:32:07 +0300

    Update InfoStrings.java

--- src/test/java/models/InfoStrings.java
+++ src/test/java/models/InfoStrings.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 public class InfoStrings {
 
-    public static String apiKey = "32d64bc90a79deabf3c0cab8c736ec6c";
+    public static String apiKey = "your_api_key";
     public static String value = "Moscow";
     public static String index = String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", value, apiKey);
 
